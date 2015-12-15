0f78323eea4178e9305e9ca31da3c5e9c54499d5
leorick <leorick>
2015-12-15 16:35:20 +0800

    Changing app startup script

--- src/main/resources/runapp.sh
+++ src/main/resources/runapp.sh
@@ -1,16 +1,20 @@
 ### create staging dir and move src files
 NOW=`date +%Y%m%d%H%M%S`
-LOG=/home/leoricklin/log/spark-${NOW}
-SRC=hdfs://nameservice1/hive/tlbd_upload/iserver/log/src
-STAG=hdfs://nameservice1/hive/tlbd_upload/iserver/log/${NOW}
+UNAME=leoricklin_syslog
+DBHOST=10.176.32.44:21050
+DBNAME=syslog
+LOG=/home/${UNAME}/log/spark-${NOW}
+BASEDIR=hdfs://nameservice1/hive/SYSLOG_upload
+SRC=${BASEDIR}/vcjson
+STAG=${BASEDIR}/vcjson.stag/${NOW}
 hdfs dfs -mkdir ${STAG}
-hdfs dfs -mv "${SRC}/*.log.gz" ${STAG}
+hdfs dfs -mv "${SRC}/*.log" ${STAG}
 ###
 export MAINCLASS=tw.com.chttl.iserver.Main
-export JAR=/home/leoricklin/jar/spark-iserver.jar
-export QUEUE=root.PERSONAL.leoricklin
+export JAR=hdfs:///user/${UNAME}/jar/spark-iserver.jar
+export QUEUE=root.PERSONAL.${UNAME}
 export HADOOP_CONF_DIR=/etc/hive/conf
-export JARS=file:///home/leoricklin/jar/spark-util.jar
+export JARS=hdfs:///user/${UNAME}/jar/spark-util.jar
 export MASTER=yarn-cluster
 export EXE_MEM=8g
 export EXE_CORE=2
@@ -21,9 +25,9 @@ export DRV_JAVA_OPT="'-Dspark.executor.extraClassPath=/home/cloudera/parcels/CDH
 export CONFS="--conf spark.driver.maxResultSize=2g"
 export CONFS="${CONFS}"
 export ARGS="${EXE_NUM} ${STAG}"
-export ARGS="${ARGS} hdfs://nameservice1/hive/tlbd_upload/iserver/txt/basic basic_record_stag basic_record"
-export ARGS="${ARGS} hdfs://nameservice1/hive/tlbd_upload/iserver/txt/complex complex_record_stag complex_record"
-export ARGS="${ARGS} root.PERSONAL.leoricklin jdbc:hive2://10.176.32.44:21050 tlbd leoricklin leoricklin"
+export ARGS="${ARGS} ${BASEDIR}/vcjson.basic basic_record_stag basic_record"
+export ARGS="${ARGS} ${BASEDIR}/vcjson.complex complex_record_stag complex_record"
+export ARGS="${ARGS} ${QUEUE} jdbc:hive2://${DBHOST} ${DBNAME} ${UNAME} ${UNAME}"
 ###
 spark-submit --class ${MAINCLASS} --master ${MASTER} --queue ${QUEUE} --driver-memory ${DRV_MEM} --executor-memory ${EXE_MEM} --executor-cores ${EXE_CORE} --num-executors ${EXE_NUM} --jars ${JARS} --driver-class-path '/home/cloudera/parcels/CDH/lib/hive/lib/*' --driver-java-options '-Dspark.executor.extraClassPath=/home/cloudera/parcels/CDH/lib/hive/lib/*' ${CONFS} ${JAR} ${ARGS} >> ${LOG}.log 2> ${LOG}.err
 ###
